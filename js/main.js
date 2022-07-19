a3db9adb82d484e0aae3fced2ae67f50cef91012
MD SAHI ALAM KHAN <ksahi4088@gmail.com>
2022-07-19 17:49:18 +0530

    files are uploaded

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').bxSlider({
+        auto: 'true',
+        speed: '800',
+        autostart: 'true',
+        pause: '3000',
+    });
+	$(function(){
+		$('#menu').slicknav();
+	});
+  });
